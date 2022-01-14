63ee17ec3337e1ccd742a1e7adb4f6fe6da253ed
User <User@DESKTOP-5Q5LE4T>
2022-01-14 21:26:33 +0900

    디렉토리 추가 및 테스트 케이스 추가

--- coronaBoard/src/main/java/kr/corona/controller/TestController.java
+++ coronaBoard/src/main/java/kr/corona/controller/TestController.java
@@ -1,8 +1,23 @@
 package kr.corona.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TestController {
 
+	@GetMapping("/")
+	public String getHomePage(HttpServletRequest request, Model model) {
+		model.addAttribute("message", "hello Spring boot");
+		return "home";
+	}
+	
+	@PostMapping("/board")
+	public String home() {
+		return "index";
+	}
 }
