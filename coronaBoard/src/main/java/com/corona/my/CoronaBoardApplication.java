7ade46fecc8d99ef1eb0e29ad36486a495b0b63f
IBK <IBK@1811-257-934>
2022-01-14 14:51:25 +0900

    코로나 게시판을 위한 스프링부트 프로젝트 생성

---
+++ coronaBoard/src/main/java/com/corona/my/CoronaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.corona.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaBoardApplication.class, args);
+	}
+
+}
---
+++ coronaBoard/src/test/java/com/corona/my/CoronaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corona.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
