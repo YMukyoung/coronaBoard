b30f0bd6d0a890228eaee78721b4a587a1e307cf
User <User@DESKTOP-5Q5LE4T>
2022-01-15 23:32:43 +0900

    네이버 맵 api 수정

--- coronaBoard/src/main/java/kr/corona/CoronaBoardApplication.java
+++ coronaBoard/src/main/java/kr/corona/CoronaBoardApplication.java
@@ -3,8 +3,10 @@ package kr.corona;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@PropertySource(value = {"classpath:application.naver.properties"})
 public class CoronaBoardApplication {
 
 	public static void main(String[] args) {
--- coronaBoard/src/main/java/kr/corona/controller/TestController.java
+++ coronaBoard/src/main/java/kr/corona/controller/TestController.java
@@ -2,6 +2,7 @@ package kr.corona.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TestController {
-
+	
+	@Value("${naver.client.id}")
+	private String naverClientId;
+	
 	@GetMapping("/")
 	public String getHomePage(HttpServletRequest request, Model model) {
 		model.addAttribute("message", "hello Spring boot");
@@ -22,7 +26,8 @@ public class TestController {
 	}
 	
 	@GetMapping("/naver/Map")
-	public String getNaverMap() {
+	public String getNaverMap(HttpServletRequest request, Model model) {
+		model.addAttribute("clientId", naverClientId);
 		return "naverMap";
 	}
 }
